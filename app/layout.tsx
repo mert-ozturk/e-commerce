45719629f061924f36cc516a0ec4353acf358b2a
mert-ozturk <mertozturk.seo.yonetim@gmail.com>
2024-06-06 06:48:36 +0300

    islem3

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import {ClerkProvider} from '@clerk/nextjs'
 import "./globals.css";
+import { ModalProvider } from "@/providers/modal-provider";
+ 
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,10 +17,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+ 
   return (
     <ClerkProvider> 
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ModalProvider />
+        {children}
+        </body>
     </html>
     </ClerkProvider>
   );
